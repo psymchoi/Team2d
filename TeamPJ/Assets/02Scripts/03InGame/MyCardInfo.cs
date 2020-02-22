373634b8b4f0cb11a4013c43a0719a5a50fe01c8
psymchoi <52904734+psymchoi@users.noreply.github.com>
2020-02-23 03:43:58 +0900

    test

---
+++ TeamPJ/Assets/02Scripts/03InGame/MyCardInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCardInfo : MonoBehaviour
+{
+    public Sprite[] m_myCardImg;
+    public string[] m_myCardTxt;
+
+    // 이 카드의 정보  (0 ~ 5번 닌자 중 랜덤하게 하나 추출넘버)
+    int m_myCardNum;
+    // 이 카드의 정보  (0 ~ 5번 닌자 중 랜덤하게 하나 추출넘버)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_myCardNum = Random.Range(0, m_myCardImg.Length);
+
+        this.GetComponent<Image>().sprite = m_myCardImg[m_myCardNum];
+        this.transform.GetChild(0).GetComponent<Text>().text = m_myCardTxt[m_myCardNum];
+
+        GetComponent<Button>().onClick.AddListener(delegate { Method_Card(m_myCardNum); });
+    }
+
+    void Method_Card(int cardNum)
+    {
+        // 해당 넘버 카드가 눌렸을 때 반응하게 될 함수
+        Debug.Log(cardNum);
+        // 해당 넘버 카드가 눌렸을 때 반응하게 될 함수
+    }
+
+    /// <summary>
+    /// 새로 고침 버튼 눌렀을 때 일어나는 이벤트.
+    /// </summary>
+    public void RefreshCard()
+    {
+        m_myCardNum = Random.Range(0, m_myCardImg.Length);
+
+        this.GetComponent<Image>().sprite = m_myCardImg[m_myCardNum];
+        this.transform.GetChild(0).GetComponent<Text>().text = m_myCardTxt[m_myCardNum];
+    }
+}
---
+++ TeamPJ/Assets/02Scripts/03InGame/ShopButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 새로 고침 버튼
+        this.transform.Find("RefreshBtn").
+                    GetComponent<Button>().onClick.AddListener
+                    (delegate { RefreshCard(); });
+        // 새로 고침 버튼
+
+
+        // 업그레이드 버튼
+
+        // 업그레이드 버튼
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 자식 객체에 있는 모든 카드 정보를 새로고침 해준다.
+    /// </summary>
+    public void RefreshCard()
+    {
+        for (int n = 0; n < transform.Find("MyCardSlot").childCount; n++)
+        {
+            // 카드를 새로고침 해준다.
+            transform.Find("MyCardSlot").
+            transform.GetChild(n).GetComponent<MyCardInfo>().RefreshCard();
+            // 카드를 새로고침 해준다.
+        }
+    }
+}
